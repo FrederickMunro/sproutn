005c859f42653e88c81468647abc75255d0f396b
FrederickMunro <fredmunro7@gmail.com>
2025-01-30 00:30:47 -0500

    Commit

--- src/components/Home/TableSection.tsx
+++ src/components/Home/TableSection.tsx
@@ -15,17 +15,17 @@ const TableSection = ({ title, contents, number }: Props) => {
     <div className='table-section-container'>
       <div className='table-section-header-div' onClick={() =>  setIsOpen(!isOpen)}>
         <div className='table-section-header-checkmark-div'>
-          <ItemContainer anim={'appear'} time={'20'}>
+          <ItemContainer anim={'appear'} time={'20'} threshold={0.9}>
             <p className='table-section-header-checkmark'>&#10003;</p>
             </ItemContainer>
         </div>
         <div className='table-section-header-title-div'>
-          <ItemContainer anim={'appear'} anim2={'right'} time={'10'}>
+          <ItemContainer anim={'appear'} anim2={'right'} time={'10'} threshold={0.9}>
             <h4 className='table-section-header-title-h'>{title}</h4>
           </ItemContainer>
         </div>
         <div className='table-section-header-plusminus-div'>
-          <ItemContainer anim={'appear'} time={'20'}>
+          <ItemContainer anim={'appear'} time={'20'} threshold={0.9}>
             <svg className={`table-section-header-plusminus-icon ${isOpen ? 'open' : 'close'}`} viewBox="0 0 100 100" width="25" height="25">
               <rect className="table-section-header-plusminus-line table-section-header-plusminus-horizontal" x="0" y="45" width="100" height="10" rx="5"></rect>
               <rect className="table-section-header-plusminus-line table-section-header-plusminus-vertical" x="45" y="0" width="10" height="100" rx="5"></rect>
