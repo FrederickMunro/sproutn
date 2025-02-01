15d40e304a75bd2e2af0f5283eebd9fc2438e9df
FrederickMunro <fredmunro7@gmail.com>
2025-01-31 19:32:07 -0500

    Commit

---
+++ src/components/BackToTop.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import "./Home/Home.css";
+
+const BackToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div
+      className={`back-to-top ${isVisible ? "visible" : ""}`}
+      onClick={scrollToTop}
+    >
+      <div className="back-to-top-arrow" />
+    </div>
+  );
+};
+
+export default BackToTop;
--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -1,18 +1,18 @@
 import ItemContainer from "./ItemContainer";
 
 interface Props {
-  icon?: any;
+  icon: any;
   title: string;
   desc: string;
 }
 
-const Step = ({ title, desc }: Props) => {
+const Step = ({ icon, title, desc }: Props) => {
   return (
     <ItemContainer anim={'appear'} time={'6'}>
       <div className='step-container'>
         <div className='step-triangle' />
         <div className='step'>
-            {/* {icon} */}
+            {icon}
             <div>
               <h3 className='step-h3'>{title}</h3>
               <p className='step-p'>{desc}</p>
